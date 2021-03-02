f2404b267e58dbcc56fb423b0c865a28cffefc2f
Felipe Silva <felipe.dacs@gmail.com>
2021-03-02 20:09:15 -0300

    :tv: add aula 3

--- index.php
+++ index.php
@@ -1,21 +1,14 @@
 <?php
 
-$notas = [9, 3, 10, 5, 10, 8];
+require 'Calculadora.php';
 
-echo "<p>a nota de português: $notas[0]</p>";
-echo "<p>a nota de matemática: $notas[1]</p>";
-echo "<p>a nota de geografia: $notas[2]</p>";
-echo "<p>a nota de história: $notas[3]</p>";
-echo "<p>a nota de química: $notas[4]</p>";
-echo "<p>a nota de artes: $notas[5]</p>";
+$notas = [9, 5, 10, 8];
 
-$quantidadeNotas = sizeof($notas);
+$calculadora = new Calculadora();
+$media = $calculadora->calculaMedia($notas);
 
-$soma = 0;
-for ($i = 0; $i < $quantidadeNotas; $i++) {
-    $soma = $soma + $notas[$i];
+if ($media) {
+    echo "A média é: $media";
+} else {
+    echo "Não foi possível calcular a média";
 }
-
-$media = $soma/ $quantidadeNotas;
-
-echo "A média é: $media";
