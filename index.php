74980cd9d6a3339c4697f248fd5190e9fb901452
Felipe Silva <felipe.dacs@gmail.com>
2021-03-02 20:14:23 -0300

    :tv: add aula 4

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <?php
 
-require 'Calculadora.php';
+$saldos = [
+     2500,
+     3000,
+     1000,
+     3700,
+     9000
+];
 
-$notas = [9, 5, 10, 8];
-
-$calculadora = new Calculadora();
-$media = $calculadora->calculaMedia($notas);
-
-if ($media) {
-    echo "A média é: $media";
-} else {
-    echo "Não foi possível calcular a média";
+foreach  ($saldos as $saldo) {
+     echo "<p>O saldo é $saldo</p>";
 }
+
+echo "<pre>";
+
+sort($saldos);
+
+echo "O menor saldo é $saldos[0]";
+echo "</pre>";
