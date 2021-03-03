f9ee9a50d11336b5dd3d69131ce35dc5b853d992
Felipe Silva <felipe.dacs@gmail.com>
2021-03-02 22:20:53 -0300

    :tv: add aula 12

--- index.php
+++ index.php
@@ -1,48 +1,27 @@
 <?php declare(strict_types=1);
 
-$correntistas1 = [
-  "Giovanni",
-  "João",
-  "Maria",
-  "Luis",
-  "Luisa",
-  "Rafael"
-];
-
-$correntistasNaoPagantes1 = [
-  "Luis",
-  "Luisa",
-  "Rafael",
-];
-
-$correntistasPagantes = array_diff ($correntistas1, $correntistasNaoPagantes1);
-
-echo "<pre>";
-var_dump ($correntistasPagantes);
-echo "</pre>";
-
 $correntistas = [
-  "Giovanni",
-  "João",
-  "Maria",
-  "Luis",
-  "Luisa",
-  "Rafael"
+    "Giovanni",
+    "João",
+    "Maria",
+    "Luis",
+    "Luisa",
+    "Rafael"
 ];
 
 $saldos = [
-   2500,
-   3000,
-   4400,
-   1000,
-   8700,
-   9000
+    2500,
+    3000,
+    4400,
+    1000,
+    8700,
+    9000
 ];
 
-$relacionados = array_combine($correntistas, $saldos);
+$relacionados = array_combine ($correntistas, $saldos);
 
-$relacionados["Matheus"] = 4500;
-
-echo "<pre>";
-var_dump ($relacionados);
-echo "</pre>";
+if (array_key_exists("João", $relacionados)) {
+    echo "O saldo do João é: {$relacionados["João"]}";
+} else {
+    echo "Não foi encontrado";
+}
